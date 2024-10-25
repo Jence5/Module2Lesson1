f33b9eed24d1fd1a141d4062a5b9c87295f38dbc
Jence5 <jencilj55@gmail.com>
2024-10-25 16:30:48 -0400

    first commit

---
+++ Arithmetic Operations in Daily Life.py
@@ -0,0 +1,16 @@
+#task 1
+
+bread =  5
+peanut_butter = 7
+jelly = 10
+total_cost = (bread + peanut_butter + jelly)
+
+print(total_cost)
+
+#task 2
+
+initial_amount = 100000
+interest_rate = .15
+total_amount = ((initial_amount*interest_rate)+initial_amount)
+
+print(total_amount)
---
+++ Python Naming Convention Practice.py
@@ -0,0 +1,6 @@
+# Task 1
+
+pi_value = 3.14
+user_age = 25
+user_location = "New York"
+MAX_LIMIT = 1000
